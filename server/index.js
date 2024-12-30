2829041c07221e2e1cf0715f444fb3fe110d4229
Aman Kumar <authorisedaman@gmail.com>
2024-12-30 16:08:56 +0530

    added

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://bulletin-is7s.onrender.com",
     credentials: true,
   })
 );
